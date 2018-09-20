74fa6e143a907191f6f01d7e271ddd08ed27459c
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-09-19 22:40:40 -0400

    Tree movement added
    Tree now moves side to side, turning around at the edge of the screen, and has a random chance to turn around when not at an edge.  Cooldown counter added to prevent too many reverses in a short period of time.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRight = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenDrops = 1f;
+    public int randomDelay = 0;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -leftAndRight)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else
+        {
+            if(pos.x > leftAndRight)
+            {
+                speed = -Mathf.Abs(speed);
+            }
+        }
 	}
+    private void FixedUpdate()
+    {
+        randomDelay--;
+        if (randomDelay <= 0)
+        {
+            if (Random.value < chanceToChangeDirections)
+            {
+                speed *= -1;
+                randomDelay = 10;
+            }
+        }
+    }
 }
