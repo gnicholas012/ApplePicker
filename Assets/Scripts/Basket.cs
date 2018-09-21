68a71edbd9a90fdcba511f8bc74bf230a58b84e1
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-09-20 21:21:22 -0400

    Score Added and Baskets Can Be Removed
    Textboxes added for high score and current score, points are awarded for collecting an apple (1 for red and 3 for gold), apples on screen clear when an apple is missed and one basket is removed on miss.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Basket : MonoBehaviour {
-
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+    private int score = 0;
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "Score: " + score;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
         Vector3 mousePos2D = Input.mousePosition;
         mousePos2D.z = -Camera.main.transform.position.z;
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
@@ -25,6 +33,16 @@ public class Basket : MonoBehaviour {
         GameObject collidedWith = coll.gameObject;
         if(collidedWith.tag == "Apple")
         {
+            score += 1;
+            scoreGT.text = "Score: " + score.ToString();
+
+            Destroy(collidedWith);
+        }
+        if(collidedWith.tag == "GoldApple")
+        {
+            score += 3;
+            scoreGT.text = "Score: " + score.ToString();
+
             Destroy(collidedWith);
         }
     }
