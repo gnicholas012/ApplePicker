ae82cedcb83fa6a8330634f74c25e8fe2fc05cb5
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-09-20 20:15:47 -0400

    Basket Movement Complete
    Basket now follows mouse and deletes apples it comes in contact with.

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -9,7 +9,7 @@ public class Apple : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		if(transform.position.y < bottomY)
+        if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
         }
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bask : MonoBehaviour {
+public class Basket : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,21 @@ public class Bask : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
 	}
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
 }
